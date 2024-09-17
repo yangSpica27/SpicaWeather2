5137bb9285679d398981868dbb8ce6721259571f
SPICa27 <1286939372@qq.com>
2024-09-17 20:08:42 +0800

    ## 优化天气到城市页面的转场动画性能 ## 修复天气到城市页面的转场动画可能导致的闪烁

--- app/src/main/java/me/spica/spicaweather2/ui/manager_city/ActivityManagerCity.kt
+++ app/src/main/java/me/spica/spicaweather2/ui/manager_city/ActivityManagerCity.kt
@@ -14,6 +14,7 @@ import androidx.core.view.WindowCompat
 import androidx.core.view.children
 import androidx.core.view.doOnNextLayout
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.fondesa.recyclerviewdivider.dividerBuilder
@@ -45,209 +46,211 @@ import java.util.*
  */
 @AndroidEntryPoint
 class ActivityManagerCity : MaterialActivity() {
-    companion object {
-        const val ARG_CITY_NAME = "arg_position"
-    }
+  companion object {
+    const val ARG_CITY_NAME = "arg_position"
+  }
 
-    private val layout by lazy {
-        ActivityManagerCityLayout(this)
-    }
+  private val layout by lazy {
+    ActivityManagerCityLayout(this)
+  }
 
-    // 是否已经执行过入场动画
-    private var hasDoEnterAnim = false
+  // 是否已经执行过入场动画
+  private var hasDoEnterAnim = false
 
-    private val home2ManagerView by lazy {
-        Home2ManagerView(this)
-    }
+  private val home2ManagerView by lazy {
+    Home2ManagerView(this)
+  }
 
-    private val adapter = ManagerCityAdapter()
+  private val adapter = ManagerCityAdapter()
 
-    private val viewModel by viewModels<CityManagerViewModel>()
+  private val viewModel by viewModels<CityManagerViewModel>()
 
-    private val itemTouchHelper =
-        CityItemTouchHelper(
-            isDrag = false,
-            onMove = { viewHolder, target ->
-                viewModel.moveCity(
-                    adapter.items[viewHolder.absoluteAdapterPosition].city,
-                    adapter.items[target.absoluteAdapterPosition].city,
-                )
-                Collections.swap(
-                    adapter.items,
-                    viewHolder.absoluteAdapterPosition,
-                    target.absoluteAdapterPosition,
-                )
-                adapter.notifyItemMoved(
-                    viewHolder.absoluteAdapterPosition,
-                    target.absoluteAdapterPosition,
-                )
-            },
+  private val itemTouchHelper =
+    CityItemTouchHelper(
+      isDrag = false,
+      onMove = { viewHolder, target ->
+        viewModel.moveCity(
+          adapter.items[viewHolder.absoluteAdapterPosition].city,
+          adapter.items[target.absoluteAdapterPosition].city,
         )
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        window.requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
-        setExitSharedElementCallback(MaterialContainerTransformSharedElementCallback())
-        window.sharedElementsUseOverlay = false
-        super.onCreate(savedInstanceState)
-        setContentView(layout)
-        init()
-    }
-
-    @Suppress("DEPRECATION")
-    private fun init() {
-        setSupportActionBar(layout.titleBar)
-        layout.recyclerView.layoutManager =
-            LinearLayoutManager(
-                this,
-                LinearLayoutManager.VERTICAL,
-                false,
-            )
-
-        WindowCompat.getInsetsController(this.window, window.decorView).apply {
-            isAppearanceLightStatusBars = true
-        }
-        dividerBuilder()
-            .showLastDivider()
-            .showFirstDivider()
-            .asSpace()
-            .size(12.dp.toInt())
-            .build()
-            .addTo(layout.recyclerView)
-
-        home2ManagerView.attachToRootView()
-
-        val layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        layoutManager.initialPrefetchItemCount = 10
-        layout.recyclerView.layoutManager = layoutManager
-
-        layout.recyclerView.adapter = adapter
-        itemTouchHelper.attachToRecyclerView(layout.recyclerView)
-
-        adapter.itemClickListener = { position, view ->
-            lifecycleScope.launch(Dispatchers.Default) {
-                Manager2HomeView.initFromViewRect(
-                    view,
-                    window,
-                    view.getTag(R.id.dn_theme_color) as Int? ?: Color.TRANSPARENT,
-                )
-                EventBus
-                    .getDefault()
-                    .post(MessageEvent.create(MessageType.Get2MainActivityAnim, position))
-
-                withContext(Dispatchers.Main) {
-                    if (Build.VERSION.SDK_INT >= 34) {
-                        overrideActivityTransition(OVERRIDE_TRANSITION_CLOSE, 0, 0)
-                    } else {
-                        overridePendingTransition(0, 0)
-                    }
-                    finish()
-                }
-            }
-        }
-
-        adapter.itemLongClickListener = { _, _ ->
-            viewModel.setSelectable(isSelectable = true)
-        }
-
-        adapter.addCityClickListener = {
-            startActivity(Intent(this@ActivityManagerCity, ActivityAddCity::class.java))
-        }
-
-        layout.deleteBtn.setOnClickListener {
-            if (adapter.getSelectCityNames().size == adapter.items.size) {
-                toast("至少保留一个城市")
-                return@setOnClickListener
-            }
-            viewModel.deleteCities(adapter.getSelectCityNames())
-        }
-
-        lifecycleScope.launch {
-            viewModel.allCityWithWeather.collectLatest { it ->
-
-                if (!hasDoEnterAnim) {
-                    layout.recyclerView.doOnNextLayout {
-                        layout.recyclerView.children.find { it.tag == intent.getStringExtra(ARG_CITY_NAME) }?.let { toView ->
-                            hasDoEnterAnim = true
-                            createInAnim(toView)
-                        }
-                    }
-                }
-
-                adapter.setItems(it)
-            }
-        }
-
-        lifecycleScope.launch {
-            viewModel.topTitle.collectLatest {
-                layout.titleBar.title = it
-            }
-        }
-
-        lifecycleScope.launch {
-            viewModel.isSelectable.collectLatest {
-                adapter.isSelectMode = it
-                itemTouchHelper.isDrag = it
-                if (it) {
-                    Timber.tag("ManagerCity").d("show delete button")
-                    layout.deleteBtn.show()
-                } else {
-                    Timber.tag("ManagerCity").d("hide delete button")
-                    layout.deleteBtn.hide()
-                }
-            }
-        }
-        layout.titleBar.setNavigationOnClickListener {
-            onBackPressedDispatcher.onBackPressed()
-        }
-        onBackPressedDispatcher.addCallback(
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    if (adapter.isSelectMode) {
-                        viewModel.setSelectable(false)
-                        return
-                    }
-                    backToMain()
-                }
-            },
+        Collections.swap(
+          adapter.items,
+          viewHolder.absoluteAdapterPosition,
+          target.absoluteAdapterPosition,
         )
-    }
+        adapter.notifyItemMoved(
+          viewHolder.absoluteAdapterPosition,
+          target.absoluteAdapterPosition,
+        )
+      },
+    )
 
-    @Suppress("DEPRECATION")
-    private fun backToMain() {
-        if (Build.VERSION.SDK_INT >= 34) {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    window.requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
+    setExitSharedElementCallback(MaterialContainerTransformSharedElementCallback())
+    window.sharedElementsUseOverlay = false
+    super.onCreate(savedInstanceState)
+    setContentView(layout)
+    init()
+  }
+
+  @Suppress("DEPRECATION")
+  private fun init() {
+    setSupportActionBar(layout.titleBar)
+    layout.recyclerView.layoutManager =
+      LinearLayoutManager(
+        this,
+        LinearLayoutManager.VERTICAL,
+        false,
+      )
+
+    WindowCompat.getInsetsController(this.window, window.decorView).apply {
+      isAppearanceLightStatusBars = true
+    }
+    dividerBuilder()
+      .showLastDivider()
+      .showFirstDivider()
+      .asSpace()
+      .size(12.dp.toInt())
+      .build()
+      .addTo(layout.recyclerView)
+
+    home2ManagerView.attachToRootView()
+
+    val layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+    layoutManager.initialPrefetchItemCount = 10
+    layout.recyclerView.layoutManager = layoutManager
+
+    layout.recyclerView.adapter = adapter
+    itemTouchHelper.attachToRecyclerView(layout.recyclerView)
+
+    adapter.itemClickListener = { position, view ->
+      lifecycleScope.launch(Dispatchers.Default) {
+        Manager2HomeView.initFromViewRect(
+          view,
+          window,
+          view.getTag(R.id.dn_theme_color) as Int? ?: Color.TRANSPARENT,
+        )
+        EventBus
+          .getDefault()
+          .post(MessageEvent.create(MessageType.Get2MainActivityAnim, position))
+
+        withContext(Dispatchers.Main) {
+          if (Build.VERSION.SDK_INT >= 34) {
             overrideActivityTransition(OVERRIDE_TRANSITION_CLOSE, 0, 0)
-        } else {
+          } else {
             overridePendingTransition(0, 0)
+          }
+          finish()
         }
-        val firstItemView = layout.recyclerView.findViewHolderForAdapterPosition(0)?.itemView
-        if (firstItemView != null) {
-            firstItemView.performClick()
+      }
+    }
+
+    adapter.itemLongClickListener = { _, _ ->
+      viewModel.setSelectable(isSelectable = true)
+    }
+
+    adapter.addCityClickListener = {
+      startActivity(Intent(this@ActivityManagerCity, ActivityAddCity::class.java))
+    }
+
+    layout.deleteBtn.setOnClickListener {
+      if (adapter.getSelectCityNames().size == adapter.items.size) {
+        toast("至少保留一个城市")
+        return@setOnClickListener
+      }
+      viewModel.deleteCities(adapter.getSelectCityNames())
+    }
+
+    lifecycleScope.launch {
+      viewModel.allCityWithWeather.collectLatest { it ->
+
+        if (!hasDoEnterAnim) {
+          layout.recyclerView.itemAnimator = null
+          layout.recyclerView.doOnNextLayout {
+            layout.recyclerView.children.find { it.tag == intent.getStringExtra(ARG_CITY_NAME) }?.let { toView ->
+              hasDoEnterAnim = true
+              createInAnim(toView)
+              layout.recyclerView.itemAnimator = DefaultItemAnimator()
+            }
+          }
+        }
+
+        adapter.setItems(it)
+      }
+    }
+
+    lifecycleScope.launch {
+      viewModel.topTitle.collectLatest {
+        layout.titleBar.title = it
+      }
+    }
+
+    lifecycleScope.launch {
+      viewModel.isSelectable.collectLatest {
+        adapter.isSelectMode = it
+        itemTouchHelper.isDrag = it
+        if (it) {
+          Timber.tag("ManagerCity").d("show delete button")
+          layout.deleteBtn.show()
+        } else {
+          Timber.tag("ManagerCity").d("hide delete button")
+          layout.deleteBtn.hide()
+        }
+      }
+    }
+    layout.titleBar.setNavigationOnClickListener {
+      onBackPressedDispatcher.onBackPressed()
+    }
+    onBackPressedDispatcher.addCallback(
+      object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+          if (adapter.isSelectMode) {
+            viewModel.setSelectable(false)
             return
+          }
+          backToMain()
         }
-        finish()
-    }
+      },
+    )
+  }
 
-    private fun createInAnim(toView: View) {
-        home2ManagerView.bindEndView(toView)
-        doOnMainThreadIdle({
-            startInAnim()
-        }, 200)
+  @Suppress("DEPRECATION")
+  private fun backToMain() {
+    if (Build.VERSION.SDK_INT >= 34) {
+      overrideActivityTransition(OVERRIDE_TRANSITION_CLOSE, 0, 0)
+    } else {
+      overridePendingTransition(0, 0)
     }
+    val firstItemView = layout.recyclerView.findViewHolderForAdapterPosition(0)?.itemView
+    if (firstItemView != null) {
+      firstItemView.performClick()
+      return
+    }
+    finish()
+  }
 
-    // 开始入场动画
-    private fun startInAnim() {
-        home2ManagerView.startAnim()
-    }
+  private fun createInAnim(toView: View) {
+    home2ManagerView.bindEndView(toView, layout)
+    doOnMainThreadIdle({
+      startInAnim()
+    }, 200)
+  }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_manager_city, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
+  // 开始入场动画
+  private fun startInAnim() {
+    home2ManagerView.startAnim()
+  }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.add_city) {
-            startActivity(Intent(this@ActivityManagerCity, ActivityAddCity::class.java))
-        }
-        return super.onOptionsItemSelected(item)
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    menuInflater.inflate(R.menu.menu_manager_city, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    if (item.itemId == R.id.add_city) {
+      startActivity(Intent(this@ActivityManagerCity, ActivityAddCity::class.java))
     }
+    return super.onOptionsItemSelected(item)
+  }
 }
--- app/src/main/java/me/spica/spicaweather2/view/Home2ManagerView.kt
+++ app/src/main/java/me/spica/spicaweather2/view/Home2ManagerView.kt
@@ -20,190 +20,221 @@ import android.widget.FrameLayout
 import androidx.core.animation.doOnEnd
 import androidx.core.graphics.ColorUtils
 import androidx.core.view.drawToBitmap
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import me.spica.spicaweather2.tools.dp
 import me.spica.spicaweather2.ui.main.ActivityMain
 import timber.log.Timber
 
 class Home2ManagerView : View {
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr,
+  constructor(context: Context?) : super(context)
+  constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+  constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+    context,
+    attrs,
+    defStyleAttr,
+  )
+
+  // 结束的View的位置
+  private val endRect: RectF = RectF(0f, 0f, 0f, 0f)
+
+  // 标记是否已经绑定了结束的View
+  @Volatile
+  private var hasBind = false
+
+  // 结束的View的Bitmap
+  private var toViewBitmap: Bitmap? = null
+
+  // 背景的Bitmap
+//  private var bgBitmap: Bitmap? = null
+
+  fun bindEndView(view: View, parent: ViewGroup) {
+    if (hasBind) return
+    CoroutineScope(Dispatchers.Default).launch {
+      val intArray = IntArray(2)
+      view.getLocationInWindow(intArray)
+      mPaint.color = ActivityMain.currentThemeColor
+      endRect.set(
+        intArray[0] * 1f,
+        intArray[1] * 1f,
+        intArray[0] + view.width * 1f,
+        intArray[1] + view.height * 1f,
+      )
+      toViewBitmap = view.drawToBitmap()
+      view.alpha = 0f
+//      bgBitmap = parent.drawToBitmap()
+      view.alpha = 1f
+      hasBind = true
+      if (needStartAnimWhenBindOk) {
+        startAnim()
+        needStartAnimWhenBindOk = false
+      }
+    }
+  }
+
+  private var endAction: (() -> Unit)? = null
+
+  private val mRootView: ViewGroup by lazy {
+    return@lazy (getActivityFromContext(context).window.decorView as ViewGroup)
+  }
+
+  // 从根布局中移除
+  private fun detachFromRootView() {
+    mRootView.removeView(this)
+    isAttached = false
+  }
+
+  private var drawRect = RectF(0f, 0f, 0f, 0f)
+
+  private var lastB = 0f
+
+  private var mPaint: Paint = Paint()
+
+  private val progressAnimation = ValueAnimator.ofFloat(0f, 1f).setDuration(475).apply {
+    interpolator = DecelerateInterpolator(1.5f)
+    addUpdateListener {
+      val progress = it.animatedValue as Float
+
+      // 设置绘制区域
+      drawRect.set(
+        0f + (endRect.left) * progress,
+        0f + (endRect.top) * progress,
+        width * 1f - (width - endRect.right) * progress,
+        height * 1f - (height - endRect.bottom) * progress,
+      )
+
+      if (drawRect.bottom > lastB) {
+        Timber.tag("异常数据").e("height:$height bottom:${drawRect.bottom} lastB:$lastB progress:$progress")
+      }
+      lastB = drawRect.bottom
+      postInvalidateOnAnimation()
+    }
+    doOnEnd {
+      // 动画结束后，将自己从根布局中移除
+      detachFromRootView()
+      endAction?.invoke()
+      endAction = null
+    }
+  }
+
+  // 将自己添加到根布局中
+  fun attachToRootView() {
+    layoutParams = FrameLayout.LayoutParams(
+      ViewGroup.LayoutParams.MATCH_PARENT,
+      ViewGroup.LayoutParams.MATCH_PARENT,
+    )
+    if (parent == null) {
+      mRootView.addView(this)
+    }
+//        setBackgroundColor(ColorUtils.setAlphaComponent(Color.BLACK, 0x80))
+    progressAnimation.cancel()
+    isAttached = true
+    invalidate()
+  }
+
+  // 标记，防止初始化数据未就绪时，动画开始
+  @Volatile
+  private var needStartAnimWhenBindOk = false
+
+  fun startAnim() {
+    if (!hasBind) {
+      // 未初始化完成，不执行动画，等执行完毕后再执行
+      needStartAnimWhenBindOk = true
+      return
+    }
+    if (progressAnimation.isRunning) return
+    post { progressAnimation.start() }
+    Timber.tag("动画").e("开始")
+  }
+
+  override fun onDetachedFromWindow() {
+    super.onDetachedFromWindow()
+    if (progressAnimation.isRunning) progressAnimation.cancel()
+    if (toViewBitmap?.isRecycled == false) {
+      toViewBitmap?.recycle()
+    }
+//    if (bgBitmap?.isRecycled == false) {
+//      bgBitmap?.recycle()
+//    }
+  }
+
+  private var isAttached = false
+
+  private fun getActivityFromContext(context: Context): Activity {
+    var mContext: Context? = context
+    while (mContext is ContextWrapper) {
+      if (mContext is Activity) {
+        return mContext
+      }
+      mContext = mContext.baseContext
+    }
+    throw RuntimeException("Activity not found!")
+  }
+
+  private val srcInXfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+
+  private val markerPaint = Paint().apply {
+    style = Paint.Style.FILL
+  }
+
+  private val bgPaint = Paint().apply {
+    style = Paint.Style.FILL
+  }
+
+  override fun onDraw(canvas: Canvas) {
+    super.onDraw(canvas)
+    mPaint.alpha = 255
+    // 如果动画未开始，直接绘制上一页面的bitmap作为占位防止闪烁
+    if (!progressAnimation.isRunning) {
+      if (ActivityMain.screenBitmap == null) return
+      canvas.drawBitmap(ActivityMain.screenBitmap!!, 0f, 0f, mPaint)
+      return
+    }
+//    bgBitmap?.let { canvas.drawBitmap(it, endRect.left, endRect.top, mPaint) }
+
+    // 保存图层
+    val layer: Int = canvas.saveLayer(0f, 0f, width.toFloat(), height.toFloat(), null)
+    // 绘制背景
+    mPaint.xfermode = null
+    // 圈定保留区域
+    canvas.drawRoundRect(
+      drawRect,
+      8.dp * progressAnimation.animatedFraction,
+      8.dp * progressAnimation.animatedFraction,
+      mPaint,
+    )
+    // 绘制过渡区域内容
+    mPaint.xfermode = srcInXfermode
+    if (ActivityMain.screenBitmap != null && progressAnimation.animatedFraction < .6f) {
+      canvas.drawBitmap(ActivityMain.screenBitmap!!, 0f, 0f, mPaint)
+    } else {
+      canvas.drawRect(0f, 0f, width * 1f, height * 1f, mPaint)
+    }
+
+    // 恢复图层
+    canvas.restoreToCount(layer)
+    mPaint.xfermode = null
+
+    markerPaint.color = ColorUtils.setAlphaComponent(
+      ActivityMain.currentThemeColor,
+      (progressAnimation.animatedFraction * 255).toInt(),
+    )
+    canvas.drawRoundRect(
+      drawRect,
+      8.dp * progressAnimation.animatedFraction,
+      8.dp * progressAnimation.animatedFraction,
+      markerPaint,
     )
 
-    private val endRect: RectF = RectF(0f, 0f, 0f, 0f)
-
-    private var hasBind = false
-
-    private var toViewBitmap: Bitmap? = null
-
-    fun bindEndView(view: View) {
-        if (hasBind) return
-        val intArray = IntArray(2)
-        view.getLocationInWindow(intArray)
-        endRect.set(
-            intArray[0] * 1f,
-            intArray[1] * 1f,
-            intArray[0] + view.width * 1f,
-            intArray[1] + view.height * 1f,
-        )
-        mPaint.color = ActivityMain.currentThemeColor
-        toViewBitmap = view.drawToBitmap()
-        hasBind = true
+    // 动画进度到90%时，渐显绘制下一页面的被遮挡的内容
+    if (toViewBitmap != null && progressAnimation.animatedFraction > .9f) {
+      mPaint.alpha = (255 * ((progressAnimation.animatedFraction - 0.9f) / (0.1))).toInt()
+      canvas.drawBitmap(toViewBitmap!!, endRect.left, endRect.top, mPaint)
     }
+//    canvas.drawBitmap(bgBitmap!!, 0f, 0f, mPaint)
+  }
 
-    private var endAction: (() -> Unit)? = null
-
-    private val mRootView: ViewGroup by lazy {
-        return@lazy (getActivityFromContext(context).window.decorView as ViewGroup)
-    }
-
-    // 从根布局中移除
-    private fun detachFromRootView() {
-        mRootView.removeView(this)
-        isAttached = false
-    }
-
-    private var drawRect = RectF(0f, 0f, 0f, 0f)
-
-    private var lastB = 0f
-
-    private var mPaint: Paint = Paint()
-
-    private val progressAnimation =
-        ValueAnimator.ofFloat(0f, 1f).setDuration(475).apply {
-            interpolator = DecelerateInterpolator(1.5f)
-            addUpdateListener {
-                val progress = it.animatedValue as Float
-
-                // 设置绘制区域
-                drawRect.set(
-                    0f + (endRect.left) * progress,
-                    0f + (endRect.top) * progress,
-                    width * 1f - (width - endRect.right) * progress,
-                    height * 1f - (height - endRect.bottom) * progress,
-                )
-
-                if (drawRect.bottom > lastB) {
-                    Timber
-                        .tag("异常数据")
-                        .e("height:$height bottom:${drawRect.bottom} lastB:$lastB progress:$progress")
-                }
-                lastB = drawRect.bottom
-                postInvalidateOnAnimation()
-            }
-            doOnEnd {
-                // 动画结束后，将自己从根布局中移除
-                detachFromRootView()
-                endAction?.invoke()
-                endAction = null
-            }
-        }
-
-    // 将自己添加到根布局中
-    fun attachToRootView() {
-        layoutParams =
-            FrameLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT,
-            )
-        if (parent == null) {
-            mRootView.addView(this)
-        }
-//        setBackgroundColor(ColorUtils.setAlphaComponent(Color.BLACK, 0x80))
-        progressAnimation.cancel()
-        isAttached = true
-        invalidate()
-    }
-
-    fun startAnim() {
-        if (progressAnimation.isRunning) return
-        progressAnimation.start()
-        Timber.tag("动画").e("开始")
-    }
-
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-        if (progressAnimation.isRunning) progressAnimation.cancel()
-        if (toViewBitmap?.isRecycled == false) {
-            toViewBitmap?.recycle()
-        }
-    }
-
-    private var isAttached = false
-
-    private fun getActivityFromContext(context: Context): Activity {
-        var mContext: Context? = context
-        while (mContext is ContextWrapper) {
-            if (mContext is Activity) {
-                return mContext
-            }
-            mContext = mContext.baseContext
-        }
-        throw RuntimeException("Activity not found!")
-    }
-
-    private val srcInXfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
-
-    private val markerPaint =
-        Paint().apply {
-            style = Paint.Style.FILL
-        }
-
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        mPaint.alpha = 255
-        // 如果动画未开始，直接绘制上一页面的bitmap作为占位防止闪烁
-        if (!progressAnimation.isRunning) {
-            if (ActivityMain.screenBitmap == null) return
-            canvas.drawBitmap(ActivityMain.screenBitmap!!, 0f, 0f, mPaint)
-            return
-        }
-        // 保存图层
-        val layer: Int = canvas.saveLayer(0f, 0f, width.toFloat(), height.toFloat(), null)
-        // 绘制背景
-        mPaint.xfermode = null
-        // 圈定保留区域
-        canvas.drawRoundRect(
-            drawRect,
-            8.dp * progressAnimation.animatedFraction,
-            8.dp * progressAnimation.animatedFraction,
-            mPaint,
-        )
-
-        // 绘制过渡区域内容
-        mPaint.xfermode = srcInXfermode
-        if (ActivityMain.screenBitmap != null && progressAnimation.animatedFraction < .6f) {
-            canvas.drawBitmap(ActivityMain.screenBitmap!!, 0f, 0f, mPaint)
-        } else {
-            canvas.drawRect(0f, 0f, width * 1f, height * 1f, mPaint)
-        }
-
-        // 恢复图层
-        canvas.restoreToCount(layer)
-        mPaint.xfermode = null
-
-        markerPaint.color =
-            ColorUtils.setAlphaComponent(
-                ActivityMain.currentThemeColor,
-                (progressAnimation.animatedFraction * 255).toInt(),
-            )
-        canvas.drawRoundRect(
-            drawRect,
-            8.dp * progressAnimation.animatedFraction,
-            8.dp * progressAnimation.animatedFraction,
-            markerPaint,
-        )
-
-        // 动画进度到90%时，渐显绘制下一页面的被遮挡的内容
-        if (toViewBitmap != null && progressAnimation.animatedFraction > .9f) {
-            mPaint.alpha = (255 * ((progressAnimation.animatedFraction - 0.9f) / (0.1))).toInt()
-            canvas.drawBitmap(toViewBitmap!!, endRect.left, endRect.top, mPaint)
-        }
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onTouchEvent(event: MotionEvent): Boolean = progressAnimation.animatedFraction != 1f
+  @SuppressLint("ClickableViewAccessibility")
+  override fun onTouchEvent(event: MotionEvent): Boolean = progressAnimation.animatedFraction != 1f
 }
